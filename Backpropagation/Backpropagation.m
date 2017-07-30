1bdd4bcb5740ac6fece17e6bc1354f9a0ca26d4a
Roger Mei <rogermei2017@u.northwestern.edu>
2017-07-30 02:28:53 -0500

    Create Backpropagation.m

---
+++ Backpropagation/Backpropagation.m
@@ -0,0 +1,48 @@
+% Load training data
+load('TrainingData.mat');
+%% Parameter Setup
+input_layer_size  = 400;  % 20x20 Input Images of Digits
+hidden_layer_size = 25;   % 25 hidden units
+num_labels = 10;          % 10 labels, from 1 to 10   
+                          % (note: "0" is mapped to label 10)
+
+%% ================ Initializing Weights ================
+
+fprintf('\nInitializing Neural Network Parameters ...\n')
+
+initial_Theta1 = randInitializeWeights(input_layer_size, hidden_layer_size);
+initial_Theta2 = randInitializeWeights(hidden_layer_size, num_labels);
+
+% Unroll parameters
+initial_nn_params = [initial_Theta1(:) ; initial_Theta2(:)];
+
+
+%% =================== Training Neural Network ===================
+%  To train the neural network, the function "fmincg" is used.
+%  This advanced optimizers trains cost functions efficiently with
+%  provided gradient computations.
+% 
+
+fprintf('\nTraining Neural Network... \n')
+
+%  Increasing MaxIter increases the amount of training.
+options = optimset('MaxIter', 50);
+lambda = 1;
+
+% Create "short hand" for the cost function to be minimized
+costFunction = @(p) nnCostFunction(p, ...
+                                   input_layer_size, ...
+                                   hidden_layer_size, ...
+                                   num_labels, X, y, lambda);
+
+% Now, costFunction is a function that takes in only one argument (the
+% neural network parameters)
+[nn_params, cost] = fmincg(costFunction, initial_nn_params, options);
+
+% Obtain Theta1 and Theta2 back from nn_params
+Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
+                 hidden_layer_size, (input_layer_size + 1));
+
+Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
+                 num_labels, (hidden_layer_size + 1));
+                 
